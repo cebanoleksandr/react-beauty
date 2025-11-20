635ea12341e91ebc9062f20790466e6e29934b9a
Oleksandr Cheban <cebanoleksandr@gmail.com>
2025-11-20 16:28:10 +0200

    Add auth logic

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createBrowserRouter, type RouteObject } from "react-router-dom";
+import { createBrowserRouter, redirect, type RouteObject } from "react-router-dom";
 import App from "../App";
 import NotFoundPage from "../pages/NotFoundPage";
 import HomePage from "../pages/HomePage";
@@ -8,17 +8,29 @@ import SettingsPage from "../pages/SettingsPage";
 import LoginPage from "../pages/LoginPage";
 import RegisterPage from "../pages/RegisterPage";
 
+const loader = () => {
+  const token = localStorage.getItem("token");
+  if (!token) throw redirect("/login");
+  return null;
+}
+
+const autLoader = () => {
+  const token = localStorage.getItem("token");
+  if (!!token) throw redirect("/");
+  return null;
+}
+
 export const routes: RouteObject[] = [
   {
     path: "/",
     Component: App,
     children: [
-      { path: '/', Component: HomePage },
-      { path: '/services', Component: ServicesPage },
-      { path: '/contacts', Component: ContactsPage },
-      { path: '/settings', Component: SettingsPage },
-      { path: '/login', Component: LoginPage },
-      { path: '/register', Component: RegisterPage },
+      { path: '/', loader, Component: HomePage },
+      { path: '/services', loader, Component: ServicesPage },
+      { path: '/contacts', loader, Component: ContactsPage },
+      { path: '/settings', loader, Component: SettingsPage },
+      { path: '/login', loader: autLoader, Component: LoginPage },
+      { path: '/register', loader: autLoader, Component: RegisterPage },
       { path: "*", Component: NotFoundPage },
     ],
   },
